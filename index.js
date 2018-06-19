e444b63b56ea1c00b01e2cfa80bf4c2f21ef6d5a
Alex Megalokonomos <alex@clockwork.gr>
2018-06-19 16:03:59 +0300

    Importing

--- index.js
+++ index.js
@@ -3,41 +3,36 @@ const util = require('util');
 var client = new elasticsearch.Client({
     host: 'localhost:9200',
 });
-async function testES() {
-    try {
-        const response = await client.search({
-            q: 'pants'
-        });
-        console.log(util.inspect(response.hits.hits,{ compact: false, depth: 5, breakLength: 80 }))
-    } catch (error) {
-        console.trace(error.message)
-    }
-}
-async function getBlock(blockno) {
-const response = await client.search({
-"body": {
-"from" : 0, "size" : 1000,
-"query" : {
-                "match" : { 
-                    "block_data.block_num" : blockno
-                }
-    }
-}
-})
- console.log('Block: '+blockno+' Ops: '+response.hits.hits.length);
-}
-//testES();
-async function blocks() {
-var i=25000000;
-while (true) {
-getBlock(i);
-await sleep(2);
-i++;
-}
-}
-function sleep(ms){
-    return new Promise(resolve=>{
-        setTimeout(resolve,ms)
+
+
+function sleep(ms) {
+    return new Promise(resolve => {
+        setTimeout(resolve, ms)
     })
 }
-blocks();
+
+async function getBlock(blockno) {
+    const response = await client.search({
+        "body": {
+            "from": 0,
+            "size": 1000,
+            "query": {
+                "match": {
+                    "block_data.block_num": blockno
+                }
+            }
+        }
+    });
+    console.log(response.hits.hits);
+}
+
+async function importBlocks(since) {
+    var i = since;
+    while (true) {
+        getBlock(i);
+        await sleep(1);
+        i++;
+    }
+}
+
+importBlocks(27000000);
